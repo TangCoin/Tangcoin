ac12b32ec0d2c2cc63eebbf478002bf42e473ad8
fubendong <fubendong2@163.com>
2014-01-04 01:40:47 +0800

    create tangcoin
    2014-1-4 1:40:43

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# Dogecoin [DOGE, Ð]
-http://dogecoin.com/
+# Tangcoin [DOGE, Ð]
+http://tangcoin.com/
 
-![DogeCoin](http://static.tumblr.com/ppdj5y9/Ae9mxmxtp/300coin.png)
+![TangCoin](http://static.tumblr.com/ppdj5y9/Ae9mxmxtp/300coin.png)
 
-## What is DogeCoin? - Such coin
-Dogecoin is like Bitcoin, but based on Litecoin, and also much more wow.
-http://dogecoin.com/
+## What is TangCoin? - Such coin
+Tangcoin is like Bitcoin, but based on Litecoin, and also much more wow.
+http://tangcoin.com/
 
 ## License - Much license
-DogeCoin is released under the terms of the MIT license. See [COPYING](COPYING)
+TangCoin is released under the terms of the MIT license. See [COPYING](COPYING)
 for more information or see http://opensource.org/licenses/MIT.
 
 ## Development and contributions - omg developers
@@ -17,31 +17,31 @@ their feature or bug fix is ready.
 
 ## Very Much Frequently Asked Questions
 
-### How much doge can exist?
+### How much tang can exist?
 Total of 100,000,000,000 much coins
 
-### How get doge?
+### How get tang?
 Scrypt Proof of Work
 
 1 Minute Block Targets, 4 Hour Diff Readjustments
 
 Special reward system: Random block rewards
 
-1-100,000: 0-1,000,000 Dogecoin Reward
+1-100,000: 0-1,000,000 Tangcoin Reward
 
-100,001 — 200,000: 0-500,000 Dogecoin Reward
+100,001 — 200,000: 0-500,000 Tangcoin Reward
 
-200,001 — 300,000: 0-250,000 Dogecoin Reward
+200,001 — 300,000: 0-250,000 Tangcoin Reward
 
-300,001 — 400,000: 0-125,000 Dogecoin Reward
+300,001 — 400,000: 0-125,000 Tangcoin Reward
 
-400,001 — 500,000: 0-62,500 Dogecoin Reward
+400,001 — 500,000: 0-62,500 Tangcoin Reward
 
-500,001 - 600,000: 0-31,250 Dogecoin Reward
+500,001 - 600,000: 0-31,250 Tangcoin Reward
 
 600,000+ — 10,000 Reward (flat)
 
-### Wow plz make dogecoind
+### Wow plz make tangcoind
 
     sudo apt-get install build-essential \
                          libssl-dev \
@@ -57,4 +57,4 @@ Special reward system: Random block rewards
 RPC 22555
 P2P 22556
 
-![](http://dogesay.com/wow//////such/coin)
+![](http://tangsay.com/wow//////such/coin)
--- src/main.h
+++ src/main.h
@@ -1,7 +1,7 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2012 The Bitcoin developers
 // Copyright (c) 2011-2012 Litecoin Developers
-// Copyright (c) 2013 DogeCoin Developers
+// Copyright (c) 2013 TangCoin Developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef BITCOIN_MAIN_H
@@ -33,7 +33,7 @@ static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
 static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
 static const int64 MIN_TX_FEE = 100000000;
 static const int64 MIN_RELAY_TX_FEE = MIN_TX_FEE;
-static const int64 MAX_MONEY = 10000000000 * COIN; // DogeCoin: maximum of 100B coins (given some randomness), max transaction 10,000,000,000 for now
+static const int64 MAX_MONEY = 10000000000 * COIN; // TangCoin: maximum of 100B coins (given some randomness), max transaction 10,000,000,000 for now
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 static const int COINBASE_MATURITY = 30;
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
@@ -540,7 +540,7 @@ public:
     {
         // Large (in bytes) low-priority (new, small-coin) transactions
         // need a fee.
-        return dPriority > 100 * COIN * 1440 / 250; // DogeCoin: 1440 blocks found a day. Priority cutoff is 100 dogecoin day / 250 bytes.
+        return dPriority > 100 * COIN * 1440 / 250; // TangCoin: 1440 blocks found a day. Priority cutoff is 100 tangcoin day / 250 bytes.
     }
 
     int64 GetMinFee(unsigned int nBlockSize=1, bool fAllowFree=true, enum GetMinFee_mode mode=GMF_BLOCK) const
@@ -1586,7 +1586,7 @@ public:
     bool CheckSignature()
     {
         CKey key;
-        if (!key.SetPubKey(ParseHex("040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9")))
+        if (!key.SetPubKey(ParseHex("040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f     4d4604fa08dce601aaf0f470216fe1b51850b4ace21b179c45070ac7b03a2")))
             return error("CAlert::CheckSignature() : SetPubKey failed");
         if (!key.Verify(Hash(vchMsg.begin(), vchMsg.end()), vchSig))
             return error("CAlert::CheckSignature() : verify signature failed");
